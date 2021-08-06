496cd7806332ebd82e27440ba2784919082a64fd
Mehmet <mehmetkusken@gmail.com>
2021-08-05 21:08:44 -0600

    Submit the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+
+
+  form.addEventListener('submit', addTask);
+
+
+  taskList.addEventListener('click', removeTask);
+
+  
+  function addTask(e){
+    e.preventDefault();
+
+ 
+    const newTaskDescription = document.getElementById('new-task-description').value;
+    console.log(newTaskDescription);
+
+   
+    const li = document.createElement('li');
+
+  
+    li.appendChild(document.createTextNode(newTaskDescription));
+
+ 
+    const deleteBtn = document.createElement('button');
+
+   
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+   
+    li.appendChild(deleteBtn);
+
+   
+    taskList.appendChild(li);
+
+  }
+
+  function removeTask(e) {
+    if (e.target.nodeName === 'BUTTON') {
+      if(confirm('Are you sure?')) {
+        let li = e.target.parentElement;
+        taskList.removeChild(li);
+      }
+    }
+
+  }
+
 });
